330672fbecea4cf021030f5fc2e68533dbefea7a
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-11 23:26:09 +0100

    Adds SportsType model.

---
+++ database/seeders/SportTypeSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\SportType;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use function Symfony\Component\Clock\now;
+
+class SportTypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        SportType::create(['name' => 'cycling', 'label' => 'Cycling']);
+        SportType::create(['name' => 'mountain_bike', 'label' => 'Mountain Bike']);
+        SportType::create(['name' => 'running', 'label' => 'Running']);
+        SportType::create(['name' => 'indoor_cycling', 'label' => 'Indoor Cycling']);
+        SportType::create(['name' => 'weight_training', 'label' => 'Weight Training']);
+    }
+}
