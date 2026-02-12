15341fac6dece68d3cfd69a3666fd731d584e6a5
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-12 17:26:02 +0100

    Adds fake heart rate zones.

---
+++ database/seeders/sample-data/fake_heart_rate_zones.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    ['zone_number' => 1, 'name' => 'Blue',   'min_bpm' => 50,  'max_bpm' => 110, 'color' => 'blue'],
+    ['zone_number' => 2, 'name' => 'Green',  'min_bpm' => 110, 'max_bpm' => 130, 'color' => 'green'],
+    ['zone_number' => 3, 'name' => 'Yellow', 'min_bpm' => 130, 'max_bpm' => 150, 'color' => 'yellow'],
+    ['zone_number' => 4, 'name' => 'Orange', 'min_bpm' => 150, 'max_bpm' => 170, 'color' => 'orange'],
+    ['zone_number' => 5, 'name' => 'Red',    'min_bpm' => 170, 'max_bpm' => 180, 'color' => 'red'],
+];
