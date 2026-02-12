87ffab825f514d60bd99dba820e3dbf81cf8167d
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-12 17:27:07 +0100

    Adds mapper class for sports types.

---
+++ app/Support/Parsers/Mappers/SportTypeMapper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Support\Parsers\Mappers;
+
+use App\Models\SportType;
+
+class SportTypeMapper
+{
+    protected const MAP = [
+        'MTB' => 'mountain_bike',
+        'CYCLING' => 'cycling',
+        'RUN' => 'running',
+        'OTHER_INDOOR' => 'other_indoor',
+        'STRENGTH_TRAINING' => 'weight_training',
+        'BOOTCAMP' => 'bootcamp'
+    ];
+
+    public static function map(string $external): ?SportType
+    {
+        $internal = self::MAP[$external] ?? null;
+
+        if (!$internal) {
+            return null;
+        }
+
+        return SportType::where('name', $internal)->first();
+    }
+}
