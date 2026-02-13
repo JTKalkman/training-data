8e505ae7da2ae44fc49a8a71bbbc6401edaa6247
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-13 13:18:22 +0100

    Shows message when there isn't any training data.

--- resources/js/pages/Diary/Week.vue
+++ resources/js/pages/Diary/Week.vue
@@ -1,15 +1,18 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
 import Layout from './Components/Layout.vue';
+import { computed } from 'vue';
 
 defineOptions({ layout: Layout })
 
-defineProps({
+const props = defineProps({
   trainingSessions: Object,
   year: Number,
   week: Number,
   navigation: Object
 })
+
+const hasSessions = computed(() => props.trainingSessions.length > 0);
 </script>
 
 <template>
@@ -21,18 +24,24 @@ defineProps({
       </h1>
       <Link :href="navigation.next.url">Next</Link>
     </div>
+
+    <div v-if="!hasSessions">
+      <p class="text-gray-500 text-center pt-20 pb-20">No training sessions this week.</p>
+    </div>
   
-    <ul>
-      <li v-for="session in trainingSessions" :key="session.id" class="border p-2 my-1">
-        <div>{{ session.sport_type.label }}</div>
-        <div>{{ session.started_at }}</div>
-        <div v-if="session.training_summary">
-          HR min: {{ session.training_summary.min_heart_rate }}<br/>
-          HR avg: {{ session.training_summary.avg_heart_rate }}<br/>
-          HR max: {{ session.training_summary.max_heart_rate }}
-        </div>
-      </li>
-    </ul>
+    <div v-else>
+      <ul>
+        <li v-for="session in trainingSessions" :key="session.id" class="border p-2 my-1">
+          <div>{{ session.sport_type.label }}</div>
+          <div>{{ session.started_at }}</div>
+          <div v-if="session.training_summary">
+            HR min: {{ session.training_summary.min_heart_rate }}<br/>
+            HR avg: {{ session.training_summary.avg_heart_rate }}<br/>
+            HR max: {{ session.training_summary.max_heart_rate }}
+          </div>
+        </li>
+      </ul>
+    </div>
 
   </div>
 </template>
