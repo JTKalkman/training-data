8baa04e79b33a97434c571c612c20dcd3cbaabdc
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-20 20:23:01 +0100

    Adds custom errors for the API client.

--- app/Support/API/Polar/PolarClient.php
+++ app/Support/API/Polar/PolarClient.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace app\Support\API\Polar;
+namespace App\Support\API\Polar;
 
 use App\Support\API\Contracts\ApiClientInterface;
 use Illuminate\Support\Facades\Http;
@@ -10,15 +10,34 @@ class PolarClient implements ApiClientInterface {
 
     const API_URL = 'https://www.polaraccesslink.com/v3/';
 
+    protected static function throwException($response): void
+    {
+        throw new PolarApiException(
+            'Polar API request failed: ' . $response->status() . ' ' . $response->body()
+        );
+    }
+
     public static function get(string $path, array $params = [], array $headers = []): Response
     {
-        return Http::withHeaders($headers)
+        $response = Http::withHeaders($headers)
             ->get(self::API_URL . $path, $params);
+
+        if ($response->failed()) {
+            self::throwException($response);
+        }
+
+        return $response;
     }
 
     public static function post(string $path, array $data = [], array $headers = []): Response
     {
-        return Http::withHeaders($headers)
+        $response = Http::withHeaders($headers)
             ->post(self::API_URL . $path, $data);
+
+        if ($response->failed()) {
+            self::throwException($response);
+        }
+
+        return $response;
     }
 }
