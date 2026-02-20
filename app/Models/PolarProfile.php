0c3e31548b9cc0cfbeb07cd81f3fa90b79c16468
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-20 21:38:06 +0100

    Adds missing relationships.

--- app/Models/PolarProfile.php
+++ app/Models/PolarProfile.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PolarProfile extends Model
 {
@@ -14,4 +15,9 @@ class PolarProfile extends Model
         'linked_at',
         'unlinked_at',
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
