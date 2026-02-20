02bb245232c8293cb459296b7f49225c24aa417a
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-20 12:15:33 +0100

    Creates interface for API clients.

---
+++ app/Support/API/Contracts/ApiClientInterface.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Support\API\Contracts;
+
+use Illuminate\Http\Client\Response;
+
+interface ApiClientInterface {
+
+    public static function get (string $path, array $params = []): Response;
+
+    public static function post(string $path, array $data = [], array $headers = []): Response;
+};
