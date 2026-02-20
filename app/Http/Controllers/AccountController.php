4e78d3225fc39e86073254844bf60e3cb24a9d11
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-20 20:25:54 +0100

    Creates account controller.

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class AccountController extends Controller
+{
+    public function settings()
+    {
+        return Inertia::render('Account/Settings');
+    }
+}
