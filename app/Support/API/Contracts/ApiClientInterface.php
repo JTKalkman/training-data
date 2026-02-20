b02d0a2220eb3dd98c156ec0376f095b81e100d9
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-20 12:21:03 +0100

    Adds header parameter in interface.

--- app/Support/API/Contracts/ApiClientInterface.php
+++ app/Support/API/Contracts/ApiClientInterface.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Client\Response;
 
 interface ApiClientInterface {
 
-    public static function get (string $path, array $params = []): Response;
+    public static function get(string $path, array $params = [], array $headers = []): Response;
 
     public static function post(string $path, array $data = [], array $headers = []): Response;
 };
