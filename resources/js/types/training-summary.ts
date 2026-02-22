7687d3980a7ea1a4cf2206845a3d4e9e2d08ef11
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-23 00:08:03 +0100

    Corrects interface to use semicolons.

--- resources/js/types/training-summary.ts
+++ resources/js/types/training-summary.ts
@@ -1,5 +1,5 @@
 export interface TrainingSummary {
-  'min_heart_rate': number,
-  'avg_heart_rate': number,
-  'max_heart_rate': number,
+  'min_heart_rate': number;
+  'avg_heart_rate': number;
+  'max_heart_rate': number;
 };
