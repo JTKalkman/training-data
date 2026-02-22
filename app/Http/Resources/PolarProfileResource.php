2f62180bd30f262539bd8063c8817e2ba14bb219
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-22 13:48:08 +0100

    Adds json resource for profiles.

---
+++ app/Http/Resources/PolarProfileResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Carbon\Carbon;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PolarProfileResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'linked_at' => $this->linked_at?->toDateString(),
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'unlinked_at' => $this->unlinked_at?->toDateString(),
+        ];
+    }
+}
