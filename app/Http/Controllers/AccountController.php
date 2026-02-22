2cdacf97ebbd15c7ff325e346c29514ae3c1c97d
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-22 13:44:52 +0100

    Adds Polar profile data for account settings.

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Resources\PolarProfileResource;
+use App\Models\PolarProfile;
+use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 
 class AccountController extends Controller
 {
     public function settings()
     {
-        return Inertia::render('Account/Settings');
+        $user = Auth::user();
+        $polarProfiles = PolarProfile::where('user_id', $user->id)
+            ->get();
+
+        return Inertia::render('Account/Settings', [
+            'polar_profiles' => PolarProfileResource::collection($polarProfiles),
+        ]);
     }
 }
