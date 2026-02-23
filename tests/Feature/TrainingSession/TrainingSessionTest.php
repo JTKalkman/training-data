decbb17edd1d2bb6558b726fa2c149fdd5467398
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-23 18:32:49 +0100

    Adds feature test.

---
+++ tests/Feature/TrainingSession/TrainingSessionTest.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Models\TrainingSession;
+use App\Models\User;
+
+test('test user cannot view other users session', function () {
+    $user1 = User::factory()->create();
+    $user2 = User::factory()->create();
+    
+    $session = TrainingSession::factory()
+        ->for($user1)
+        ->create();
+    
+    $response = $this->actingAs($user2)
+        ->get(route('sessions.session', $session));
+    
+    $response->assertStatus(403);
+});
+
+test('test user can view own session', function() {
+    $user = User::factory()->create();
+    
+    $session = TrainingSession::factory()
+        ->for($user)
+        ->create();
+    
+    $response = $this->actingAs($user)
+        ->get(route('sessions.session', $session));
+    
+    $response->assertStatus(200);
+
+});
