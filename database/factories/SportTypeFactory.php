131895c9b672312780a412982a4a94688199085b
J.T. Kalkman <mail@jtkalkman.nl>
2026-02-27 15:25:45 +0100

    Updates factories and seeders.

--- database/factories/SportTypeFactory.php
+++ database/factories/SportTypeFactory.php
@@ -2,7 +2,6 @@
 
 namespace Database\Factories;
 
-use App\Models\SportType;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
--- database/seeders/DataSourceSeeder.php
+++ database/seeders/DataSourceSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\DataSource;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class DataSourceSeeder extends Seeder
