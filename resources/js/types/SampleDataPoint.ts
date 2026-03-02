706d14cb8f9748b91d66651a08aa1ae2841d2292
J.T. Kalkman <mail@jtkalkman.nl>
2026-03-02 21:51:01 +0100

    Creates TypeScript interfaces for chart data.

---
+++ resources/js/types/SampleDataPoint.ts
@@ -0,0 +1,9 @@
+export interface SampleDataPoint {
+  time: number;
+  time_label: string;
+  heart_rate: number;
+  speed?: number;
+  cadence?: number;
+  altitude?: number;
+  distance?: number;
+};
