1f5accbc49b83f0d944b0468b26d3c0d4454ebb5
J.T. Kalkman <mail@jtkalkman.nl>
2026-03-04 16:00:22 +0100

    Removes downsampling from map data request.

--- app/Support/MapData.php
+++ app/Support/MapData.php
@@ -7,24 +7,6 @@ use Illuminate\Support\Facades\Storage;
 
 class MapData
 {
-    // TMP: Very simple downsampling. Use RDP of Visvalingam for more accurate results.
-    private static function downSample(array $coordinates, int $nth = 5): array
-    {
-        $result = [];
-    
-        foreach ($coordinates as $i => $coordinate) {
-            if ($i % $nth === 0) {
-                $result[] = $coordinate;
-            }
-        }
-
-        if (end($coordinates) !== end($result)) {
-            $result[] = end($coordinates);
-        }
-
-        return $result;
-    }
-
     public static function fromSession(TrainingSession $session): ?string
     {
         $path = $session->routeDataPath();
@@ -33,9 +15,6 @@ class MapData
             return null;
         }
 
-        $coordinates = json_decode(Storage::get($path), true);
-        $downSampled = self::downSample($coordinates);
-
-        return json_encode($downSampled);
+        return Storage::get($path);
     }
 }
