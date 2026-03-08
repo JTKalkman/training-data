8acfdc6d48e0a32bff1b661447fbc0503e224ce1
J.T. Kalkman <mail@jtkalkman.nl>
2026-03-08 12:17:33 +0100

    Adds endpoint for list of training sessions.

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\AuthController;
 use App\Http\Controllers\Api\V1\ProfileController;
 use App\Http\Controllers\Api\V1\DeviceController;
+use App\Http\Controllers\Api\V1\TrainingSessionController;
 
 Route::post('/auth/login', [AuthController::class, 'login']);
 
@@ -16,17 +17,22 @@ Route::middleware('auth:sanctum')->group(function() {
     Route::get('profiles', [ProfileController::class, 'index']);
     // Route::delete('profiles/{}', []); // TODO
     // Route::put('profiles/{}/restore', []); // TODO
+
     Route::get('devices', [DeviceController::class, 'index']);
+
+    Route::get('training-sessions', [TrainingSessionController::class, 'index']);
+    // Route::get('training-sessions/{id} ', []);
+    // Route::get('training-sessions/training-sessions/{id}/sample-data', []);
+    // Route::get('/training-sessions/{id}/route', []);
+    // Route::patch('/training-sessions/{id} ', []);
+    // Route::get('training-sessions', []);
 });
 
 /**
  * TODO: 
  * 
- * Rate limiting.
  * 
  * Authenticated:
- * GET      /api/v1/me/devices                                                      The users devices. No pagination because we expect a small number of devices.
- * GET      /api/v1/training-sessions                                               All training sessions in the last 30 days with sports types and summaries.
  * GET      /api/v1/training-sessions?from=2026-03-01&to=2026-03-07&sport=running   All training sessions with filtering. Paginated.
  * GET      /api/v1/training-sessions/{id}                                          The current training session with sport type, training summary and heart rate zones.
  * GET      /api/v1/training-sessions/{id}/sample-data                              The sample data for the current training session. Large json file from storage.
