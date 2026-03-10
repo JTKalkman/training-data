7859b4ccc02cc84feb1a904f50bd0ae7d9b5ff36
J.T. Kalkman <mail@jtkalkman.nl>
2026-03-10 16:01:13 +0100

    Creates job for daily sync. Schedules daily sync.

---
+++ app/Jobs/SyncPolarExercisesJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\PolarProfile;
+use App\Support\PolarExerciseSync;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Events\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SyncPolarExercisesJob implements ShouldQueue
+{
+    use InteractsWithQueue, Queueable, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct(
+        private PolarProfile $polarProfile
+    ) {}
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        PolarExerciseSync::run($this->polarProfile->user);
+    }
+}
