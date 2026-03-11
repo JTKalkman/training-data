afa1bba40c1cdf9a09b9dfbe9f152ef54b8a6988
J.T. Kalkman <mail@jtkalkman.nl>
2026-03-11 18:57:42 +0100

    Removes unused imports.

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3';
-import { dashboard, login, register } from '@/routes';
+import { dashboard, login, 
+    // register 
+} from '@/routes';
 
 withDefaults(
     defineProps<{
