09918b0122c3bcece2a8fb75ef63f416942c5f61
J.T. Kalkman <mail@jtkalkman.nl>
2026-03-15 22:34:32 +0100

    Adds ratelimiting.

--- routes/api/v1/api.php
+++ routes/api/v1/api.php
@@ -7,23 +7,25 @@ use App\Http\Controllers\Api\V1\SportTypeController;
 use App\Http\Controllers\Api\V1\TrainingSessionController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/auth/login', [AuthController::class, 'login']);
+Route::middleware(['throttle:api'])->group(function () {
+    Route::post('/auth/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/auth/refresh', [AuthController::class, 'refresh']);
-    Route::delete('/auth/logout', [AuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::post('/auth/refresh', [AuthController::class, 'refresh']);
+        Route::delete('/auth/logout', [AuthController::class, 'logout']);
 
-    Route::get('profiles', [ProfileController::class, 'index']);
-    // Route::delete('profiles/{}', []); // TODO: Soft deletes, keeps data intact.
-    // Route::put('profiles/{}/restore', []); // TODO: Restore if deleted by mistake.
+        Route::get('profiles', [ProfileController::class, 'index']);
+        // Route::delete('profiles/{}', []); // TODO: Soft deletes, keeps data intact.
+        // Route::put('profiles/{}/restore', []); // TODO: Restore if deleted by mistake.
 
-    Route::get('devices', [DeviceController::class, 'index']);
+        Route::get('devices', [DeviceController::class, 'index']);
 
-    Route::get('training-sessions', [TrainingSessionController::class, 'index']);
-    Route::get('training-sessions/{trainingSession} ', [TrainingSessionController::class, 'show']);
-    Route::get('training-sessions/{trainingSession}/sample-data', [TrainingSessionController::class, 'sampleData']);
-    Route::get('training-sessions/{trainingSession}/route-data', [TrainingSessionController::class, 'routeData']);
-    // Route::patch('/training-sessions/{id} ', []); // TODO: Update or add feedback or notes.
+        Route::get('training-sessions', [TrainingSessionController::class, 'index']);
+        Route::get('training-sessions/{trainingSession} ', [TrainingSessionController::class, 'show']);
+        Route::get('training-sessions/{trainingSession}/sample-data', [TrainingSessionController::class, 'sampleData']);
+        Route::get('training-sessions/{trainingSession}/route-data', [TrainingSessionController::class, 'routeData']);
+        // Route::patch('/training-sessions/{id} ', []); // TODO: Update or add feedback or notes.
 
-    Route::get('sport-types', [SportTypeController::class, 'index']);
+        Route::get('sport-types', [SportTypeController::class, 'index']);
+    });
 });
