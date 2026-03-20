d14bbade6a33e6f1f781f4107e800e1dc95fb61a
J.T. Kalkman <mail@jtkalkman.nl>
2026-03-20 21:12:25 +0100

    Improves layout.

--- resources/js/pages/Week.vue
+++ resources/js/pages/Week.vue
@@ -58,21 +58,32 @@ const breadcrumbs: BreadcrumbItem[] = [
       <div v-if="!hasSessions">
         <p class="text-gray-500 text-center pt-20 pb-20">No training sessions this week.</p>
       </div>
-    
+
       <div v-else>
-        <ul>
-          <li v-for="session in trainingSessions?.data" :key="session.id" class="border p-2 my-1">
-  
-            <div>
-              <Link :href="route('training-sessions.session', { session: session.id })" class="underline">
-                {{ session.sportType.label }} — {{ session.startedAtHuman }}
+        <ul class="border rounded-xl">
+          <li 
+            v-for="session in trainingSessions?.data" :key="session.id" 
+            class="relative p-4 border-b first:rounded-t-xl last:border-0 last:rounded-b-xl hover:bg-sidebar-accent dark:hover:bg-sidebar-accent flex items-center"
+          >
+
+            <div class="bg-red-700 rounded-lg w-12 h-12 mr-4"></div>
+
+            <div class="lg:flex-1">
+              <div class="grid grid-cols-1 lg:grid-cols-3 lg:items-center w-full">
+                <p class="font-medium lg:w-1/3 text-nowrap">{{ session.sportType.label }}</p>
+                <p class="text-sm text-gray-600 dark:text-gray-400 lg:w-1/3 text-nowrap">
+                  {{ session.startedAtHuman }}
+                </p>
+                <p class="text-sm text-gray-600 dark:text-gray-400 lg:w-1/3 text-nowrap">
+                  <span v-if="session.durationHuman">{{ session.durationHuman }}</span>
+                </p>
+              </div>
+
+              <Link :href="route('training-sessions.session', { session: session.id })" class="after:absolute after:inset-0">
+                <span class="sr-only">View details for {{ session.sportType.label }} session on {{ session.startedAtHuman }}</span>
               </Link>
             </div>
-  
-            <div v-if="session.durationHuman">
-              {{ session.durationHuman }}
-            </div>
-  
+
           </li>
         </ul>
       </div>
