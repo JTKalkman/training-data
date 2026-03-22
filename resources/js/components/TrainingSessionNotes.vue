72a67e0a9ab93f60a5f1b713375053e835c4f67a
J.T. Kalkman <mail@jtkalkman.nl>
2026-03-22 20:46:32 +0100

    Uses text area component.

--- resources/js/components/TrainingSessionNotes.vue
+++ resources/js/components/TrainingSessionNotes.vue
@@ -1,15 +1,16 @@
 <script setup lang="ts">
 import { cn } from '@/lib/utils';
 import { router, useForm } from '@inertiajs/vue3';
-import { Check, CheckCheck, X } from 'lucide-vue-next';
+import { Check, X } from 'lucide-vue-next';
 import { computed, HTMLAttributes, onBeforeUnmount, onMounted } from 'vue';
 import { route } from 'ziggy-js';
 import Button from './ui/button/Button.vue';
+import TextArea from './ui/text-area/TextArea.vue';
 
 const props = defineProps<{
   class?: HTMLAttributes["class"];
   trainingSessionId: string;
-  notes?: string;
+  notes?: string | number | null;
 }>();
 
 const notesChanged = computed(() => (form.notes ?? '') !== (props.notes ?? ''))
@@ -29,6 +30,10 @@ const submit = () => {
   });
 }
 
+const handleChange = (value: string | number | null) => {
+  form.notes = value
+}
+
 const removeListener = router.on('before', (event) => {
   if (form.processing) {
     event.preventDefault()
@@ -44,14 +49,17 @@ onBeforeUnmount(() => removeListener())
     @keydown.esc="cancel"
     :class="cn('flex flex-col space-y-2', props.class)"
   >
-    <textarea
-      v-model="form.notes"
+ 
+    <TextArea 
       placeholder="Additional notes..."
-      class="border rounded-sm text-sm overflow-y-scroll min-h-10 p-2 lg:grow mb-0"
+      :modelValue="form.notes"
       :disabled="form.processing"
       name="notes"
       id="notes"
+      class="min-h-10 lg:grow"
+      v-on:update:modelValue="handleChange"
     />
+
     <div v-if="notesChanged" class="space-x-2 flex justify-end mt-1">
       <Button 
         :variant="'secondary'"
