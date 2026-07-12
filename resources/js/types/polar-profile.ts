859fca744314fac0c250378e56106cf8a7e51c97
J.T. Kalkman <mail@jtkalkman.nl>
2026-07-12 13:28:48 +0200

    Extends type.

--- resources/js/types/polar-profile.ts
+++ resources/js/types/polar-profile.ts
@@ -4,4 +4,6 @@ export interface PolarProfile {
     last_name: string | null;
     linked_at: string;
     unlinked_at: string | null;
+    last_synced_at: string | null;
+    sync_status: 'ok' | 'warning' | 'error' | 'pending';
 };
