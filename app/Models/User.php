13734b41f827695f70c2aabe739bed41fed944ca
J.T. Kalkman <mail@jtkalkman.nl>
2026-07-12 22:11:25 +0200

    Add access for admin users to telescope.

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Gate;
 use Laravel\Fortify\TwoFactorAuthenticatable;
 use Laravel\Sanctum\HasApiTokens;
 
@@ -65,5 +66,10 @@ class User extends Authenticatable
     public function devices(): HasMany
     {
         return $this->hasMany(Device::class);
+    }
+
+    protected function gate(): void
+    {
+        Gate::define('viewTelescope', fn (User $user) => $user->is_admin);
     }
 }
