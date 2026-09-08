71acaa50eab75428e782fc7724abc6b92f8ba77a
J.T. Kalkman <mail@jtkalkman.nl>
2026-09-08 22:37:24 +0200

    Allows for null values for devices.

--- app/Support/Parsers/ParsedDeviceData.php
+++ app/Support/Parsers/ParsedDeviceData.php
@@ -4,13 +4,13 @@ namespace App\Support\Parsers;
 
 class ParsedDeviceData
 {
-    public string $name;
+    public null|string $name;
 
-    public string $externalId;
+    public null|string $externalId;
 
     public function __construct(array $data)
     {
-        $this->name = $data['name'];
-        $this->externalId = $data['external_id'];
+        $this->name = $data['name'] ?? null;
+        $this->externalId = $data['external_id'] ?? null;
     }
 }
