3f0c84755c4034460cb1ab30805ad599feb0ded0
J.T. Kalkman <mail@jtkalkman.nl>
2026-09-16 16:04:41 +0200

    Remove temp comment.

--- resources/js/types/hover-position.ts
+++ resources/js/types/hover-position.ts
@@ -1,6 +1,3 @@
-/**
- * Maybe expand this a bit more to show heart rate, speed, etc.. on the map or to pass data more easily?
- */
 export interface HoverPosition {
   time: number;
   index: number;
