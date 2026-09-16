61439827cdc4730baf93fcdfd1030dad933a9adf
J.T. Kalkman <mail@jtkalkman.nl>
2026-09-16 23:21:29 +0200

    Add recenter option to maps. Use custom zoom controls.

--- resources/js/components/Map.vue
+++ resources/js/components/Map.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
 import * as L from 'leaflet';
-import { onMounted, ref, watch } from 'vue';
+import { onMounted, onUnmounted, ref, watch } from 'vue';
 import type { RouteDataPoint } from '@/types';
 import 'leaflet/dist/leaflet.css';
+import Button from './ui/button/Button.vue';
 
 const props = defineProps<{
   data: Array<RouteDataPoint>;
@@ -11,21 +12,31 @@ const props = defineProps<{
 
 const mapContainer = ref<HTMLElement | null>(null);
 let mapInstance: L.Map;
+let polyline: L.Polyline;
 let hoverMarker: L.CircleMarker | null = null;
 
-const createMap = () => {
-  const coordinates = props.data.map(point => [point.lat, point.lng]);
+const getCoordinates = (): L.LatLngExpression[] => {
+  return props.data.map(point => [point.lat, point.lng]);
+}
 
-  mapInstance = L
-    .map(mapContainer.value!, { scrollWheelZoom: false })
-    .setView([coordinates[0][0], coordinates[0][1]], 13);
-  L
-    .tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png')
-    .addTo(mapInstance);
-  const polyline = L
-    .polyline(coordinates)
-    .addTo(mapInstance);
-  mapInstance.fitBounds(polyline.getBounds());
+const createMap = () => {
+  const coordinates = getCoordinates();
+
+  mapInstance = L.map(mapContainer.value!, {
+    scrollWheelZoom: false,
+    zoomSnap: 0.1,
+    zoomControl: false,
+  });
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(mapInstance);
+
+  polyline = L.polyline(coordinates).addTo(mapInstance);
+
+  recenter()
+}
+
+const recenter = () => {
+  mapInstance.fitBounds(polyline.getBounds(), { padding: [20, 20], maxZoom: 15 });
 }
 
 watch(() => props.markerPosition, (position) => {
@@ -50,8 +61,49 @@ watch(() => props.markerPosition, (position) => {
 })
 
 onMounted(() => createMap())
+
+onUnmounted(() => {
+  mapInstance?.remove();
+});
+
 </script>
 
 <template>
-  <div style="width: 100%; height: 300px;" ref="mapContainer"></div>
+  <div class="relative">
+    <div style="width: 100%; height: 300px;" ref="mapContainer"></div>
+
+    <div class="absolute top-2 right-2 z-1000 flex flex-col gap-1">
+
+      <div class="bg-gray-400 rounded-lg">
+        <Button
+          variant="secondary"
+          type="button"
+          title="Zoom in"
+          @click="mapInstance.zoomIn()"
+          class="w-8 h-8 p-0 border border-gray-400 dark:border-gray-800 shadow"
+        >+</Button>
+      </div>
+
+      <div class="bg-gray-400 rounded-lg">
+        <Button
+          variant="secondary"
+          type="button"
+          title="Zoom out"
+          @click="mapInstance.zoomOut()"
+          class="w-8 h-8 p-0 border border-gray-400 dark:border-gray-800 shadow"
+          >-</Button>
+      </div>
+
+      <div class="bg-gray-400 rounded-lg">
+        <Button
+          variant="secondary"
+          type="button"
+          title="Recenter"
+          @click="recenter"
+          class="w-8 h-8 p-0 border border-gray-400 dark:border-gray-800 shadow"
+        >⟲</Button>
+      </div>
+
+    </div>
+  </div>
 </template>
