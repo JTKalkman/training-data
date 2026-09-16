7ffb7fbc4e81e6f1d148bce6190443119282d42a
J.T. Kalkman <mail@jtkalkman.nl>
2026-09-16 11:05:01 +0200

    Add time to RouteDataPoint.

--- resources/js/types/hover-position.ts
+++ resources/js/types/hover-position.ts
@@ -1,4 +1,8 @@
+/**
+ * Maybe expand this a bit more to show heart rate, speed, etc.. on the map or to pass data more easily?
+ */
 export interface HoverPosition {
+  time: number;
   index: number;
   x: number;
 }
