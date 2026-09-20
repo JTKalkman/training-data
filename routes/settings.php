8b6c3e6173c631d1585a4f12d6f03194dae5b5b0
J.T. Kalkman <mail@jtkalkman.nl>
2026-09-21 00:03:26 +0200

    Add routes for the training settings.

--- routes/settings.php
+++ routes/settings.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Settings\PasswordController;
 use App\Http\Controllers\Settings\ProfileController;
+use App\Http\Controllers\Settings\TrainingSettingController;
 use App\Http\Controllers\Settings\TwoFactorAuthenticationController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -11,6 +12,9 @@ Route::middleware(['auth'])->group(function () {
 
     Route::get('settings/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('settings/profile', [ProfileController::class, 'update'])->name('profile.update');
+
+    Route::get('settings/training-settings', [TrainingSettingController::class, 'edit'])->name('training-settings.edit');
+    Route::patch('settings/training-settings', [TrainingSettingController::class, 'update'])->name('training-settings.update');
 });
 
 Route::middleware(['auth', 'verified'])->group(function () {
