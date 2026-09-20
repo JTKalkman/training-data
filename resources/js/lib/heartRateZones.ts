746adeb1432fdf2100dbd66ac200a9d3d681455d
J.T. Kalkman <mail@jtkalkman.nl>
2026-09-20 11:12:35 +0200

    Takes out of bound values into account when getting the heart rate zone.

--- resources/js/lib/heartRateZones.ts
+++ resources/js/lib/heartRateZones.ts
@@ -1,17 +1,69 @@
-import { HeartRateZone } from "@/types";
+import { ColorConfig, HeartRateZone } from "@/types";
 
-export function getZoneForHeartRate(bpm: number, zones: HeartRateZone[]): HeartRateZone | null {
-  return zones.find(z => bpm >= z.min_bpm && bpm <= z.max_bpm) ?? null;
-}
-
-const zoneColorClassesMap: Record<string, string> = {
-  blue: 'bg-blue-500',
-  green: 'bg-green-500',
-  yellow: 'bg-yellow-500',
-  orange: 'bg-orange-500',
-  red: 'bg-red-500',
+const heartRateZoneColorMap: Record<string, ColorConfig> = {
+  blue: {
+    hex: {
+      foreground_color: '#00a6f4',
+      background_color: '#b8e6fe'
+    },
+    tailwind: {
+      foreground_color: 'bg-sky-500',
+      background_color: 'bg-sky-200'
+    }
+  },
+  green: {
+    hex: {
+      foreground_color: '#00c950',
+      background_color: '#b9f8cf'
+    },
+    tailwind: {
+      foreground_color: 'bg-green-500',
+      background_color: 'bg-green-200'
+    }
+  },
+  yellow: {
+    hex: {
+      foreground_color: '#f0b100',
+      background_color: '#fff085'
+    },
+    tailwind: {
+      foreground_color: 'bg-yellow-500',
+      background_color: 'bg-yellow-200'
+    }
+  },
+  orange: {
+    hex: {
+      foreground_color: '#ff6900',
+      background_color: '#ffd6a7'
+    },
+    tailwind: {
+      foreground_color: 'bg-orange-500',
+      background_color: 'bg-orange-200'
+    }
+  },
+  red: {
+    hex: {
+      foreground_color: '#fb2c36',
+      background_color: '#ffc9c9'
+    },
+    tailwind: {
+      foreground_color: 'bg-red-500',
+      background_color: 'bg-red-200'
+    }
+  },
 };
 
-export const zoneColorClasses = (color: string): string | null => {
-  return zoneColorClassesMap[color] ?? null;
+export const getZoneForHeartRate = (bpm: number, zones: HeartRateZone[]): HeartRateZone | null => {
+  if (!zones.length) return null;
+
+  const sorted = [...zones].sort((a, b) => a.min_bpm - b.max_bpm);
+
+  if (bpm < sorted[0].min_bpm) return sorted[0]; // Below any zone.
+  if (bpm > sorted[sorted.length - 1].max_bpm) return sorted[sorted.length - 1]; // Above all zones.
+
+  return sorted.find(z => bpm >= z.min_bpm && bpm <= z.max_bpm) ?? null;
+}
+
+export const heartRateZoneColors = (color: string): ColorConfig | null => {
+  return heartRateZoneColorMap[color] ?? null;
 }
