0ae9bfdc5d144782daab3af1391ee6c310cfa4e3
J.T. Kalkman <mail@jtkalkman.nl>
2026-09-26 20:29:47 +0200

    Add sex and vo2 max to validation rules.

--- app/Concerns/PhysicalDataValidationRules.php
+++ app/Concerns/PhysicalDataValidationRules.php
@@ -2,7 +2,9 @@
 
 namespace App\Concerns;
 
-trait TrainingSettingsValidationRules
+use Illuminate\Validation\Rule;
+
+trait PhysicalDataValidationRules
 {
     protected const validHeartRate = ['nullable', 'integer', 'min:50', 'max:300'];
 
@@ -11,16 +13,18 @@ trait TrainingSettingsValidationRules
      *
      * @return array<string, array<int, \Illuminate\Contracts\Validation\Rule|array<mixed>|string>>
      */
-    protected function trainingSettingsRules(): array
+    protected function physicalDataRules(): array
     {
         return [
             'date_of_birth'           => ['nullable', 'date'],
             'weight_kg'               => ['nullable', 'decimal:1', 'min:0', 'max:1000'],
             'height_cm'               => ['nullable', 'integer', 'min:50', 'max:300'],
+            'sex'                     => ['nullable', 'string', Rule::in(['male', 'female', 'prefer_not_to_say']),],
             'resting_heart_rate'      => [...self::validHeartRate, 'lt:max_heart_rate'],
             'max_heart_rate'          => [...self::validHeartRate, 'gt:resting_heart_rate'],
             'aerobic_threshold_bpm'   => [...self::validHeartRate, 'lt:anaerobic_threshold_bpm'],
             'anaerobic_threshold_bpm' => [...self::validHeartRate, 'gt:aerobic_threshold_bpm'],
+            'vo2_max'                 => ['nullable', 'integer', 'min:0', 'max:100'],
             'mas_pace'                => ['nullable', 'regex:/^\d+:[0-5]\d$/'],
             'mas_seconds_per_km'      => ['nullable', 'integer', 'min:0', 'max:1200'],
             'ftp_watts'               => ['nullable', 'integer', 'min:1', 'max:65535', 'lt:map_watts'],
